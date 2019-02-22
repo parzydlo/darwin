47e0f3afbee59b4881a4629f0b05a18979058761
Jan Parzydło <jan@panbog.local>
2019-02-22 01:26:11 +0000

    add basic tests for CFG and GNF2CNF

---
+++ test/test_cfg.rb
@@ -0,0 +1,32 @@
+require_relative './cfg.rb'
+require_relative './grammar_rule.rb'
+require_relative './grammar_symbol.rb'
+
+# build following grammar:
+# A -> x B A | eps
+# B -> y B
+
+t_x = GrammarSymbol.new(:terminal, 'x')
+t_y = GrammarSymbol.new(:terminal, 'y')
+
+nt_a = GrammarSymbol.new(:nonterminal, 0)
+nt_b = GrammarSymbol.new(:nonterminal, 1)
+
+r1 = GrammarRule.new(nt_a, [t_x, nt_b, nt_a])
+r2 = GrammarRule.new(nt_b, [t_y, nt_b])
+
+grammar = CFG.new([r1, r2])
+print grammar
+
+nt_c = GrammarSymbol.new(:nonterminal, 2)
+r1_replacement = GrammarRule.new(nt_a, [t_x, nt_b, nt_a, nt_c])
+r3 = GrammarRule.new(nt_c, [t_x, nt_c])
+
+grammar.remove_rule(grammar.rules[0])
+grammar.add_rule(r1_replacement)
+grammar.add_rule(r3)
+print grammar
+p grammar.nt_count
+p "deepcopy:"
+print grammar.deepcopy.to_s
+p grammar.equal?(grammar.deepcopy)
---
+++ test/test_gnf2cnf.rb
@@ -0,0 +1,30 @@
+require_relative './cfg.rb'
+require_relative './grammar_rule.rb'
+require_relative './grammar_symbol.rb'
+require_relative './gnf2cnf.rb'
+
+# create following GNF grammar:
+# A -> x B A | eps
+# B -> y B
+
+nt_a = GrammarSymbol.new(:nonterminal, 0)
+nt_b = GrammarSymbol.new(:nonterminal, 1)
+t_x = GrammarSymbol.new(:terminal, 'x')
+t_y = GrammarSymbol.new(:terminal, 'y')
+
+r1 = GrammarRule.new(nt_a, [t_x, nt_b, nt_a])
+r2 = GrammarRule.new(nt_b, [t_y, nt_b])
+
+gnf_grammar = CFG.new([r1, r2])
+p gnf_grammar
+
+# call GNF2CNF#convert to obtain CNF grammar
+
+cnf_grammar = GNF2CNF.convert(gnf_grammar)
+print cnf_grammar
+# desired output:
+# A -> C E
+# B -> D B
+# D -> y
+# C -> x
+# E -> B A
